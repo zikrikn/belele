84a2e192232bebc5acbd33315a8f8d5f81eda1eb
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2022-11-04 16:26:12 +0700

    1.2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware # Untuk CORS Middleware beda tempat
+from fastapi import FastAPI, File, UploadFile 
+# File dan UploadFile untuk Drive
+# For Drive
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware # Untuk CORS Middleware beda tempat. Pakai Fetch & JS untuk implementasinya
 from schemas.admin import Admin
 from schemas.kolam import *
 from db import *
 
-#Fokus untuk membuat REST API-nya buat backend
+# Fokus untuk membuat REST API-nya buat backend
 
 app = FastAPI(
     title="LeMES",
     version="1.0",
     prefix="/api"
 )
+
+# For Drive Images
+drive = deta.Drive("images")
 
 @app.get("/hi")
 async def main():
@@ -29,6 +35,27 @@ def create_user(user: Admin):
     pengguna = db_admin.put(user.dict())
     return pengguna
 
+@app.get("/imgs", response_class=HTMLResponse)
+def render():
+    return """
+    <form action="/upload" enctype="multipart/form-data" method="post">
+        <input name="file" type="file">
+        <input type="submit">
+    </form>
+    """
+
+@app.post("/upload")
+def upload_img(file: UploadFile = File(...)):
+    name = file.filename
+    f = file.file
+    res = drive.put(name, f)
+    return res
+
+@app.get("/download/{name}")
+def download_img(name: str):
+    res = drive.get(name)
+    return StreamingResponse(res.iter_chunks(1024), media_type="image/png")
+
 app.add_middleware(
     CORSMiddleware,
     #allow_origins=['http://app.lemes.my.id', 'https://app.lemes.my.id'],
@@ -37,3 +64,4 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
